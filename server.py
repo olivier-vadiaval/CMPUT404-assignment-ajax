7d2721227cfe134502a42e7b3df3f0631188b04a
olivier-vadiaval <vadiaval@ualberta.ca>
2021-11-02 12:15:58 -0600

    Complete API for clear and get entity. Simplify update api

--- server.py
+++ server.py
@@ -79,15 +79,9 @@ def hello():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    # return None
     data = flask_post_json()
     try:
-        if myWorld.get(entity) != {}:
-            for key in data[entity].keys():
-                myWorld.update(entity, key, data[entity][key])
-        else:
-            myWorld.set(entity, data)
-
+        myWorld.set(entity, data)
         return json.dumps(data), 200
     except:
         return '', 500
@@ -100,12 +94,13 @@ def world():
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity)), 200
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.dumps(myWorld.world()), 200
 
 if __name__ == "__main__":
     app.run()
